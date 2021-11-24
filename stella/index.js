4c942660cb5ee581383268dae16abe65208e617f
BL4CK H47 <rizaddq789654@gmail.com>
2021-11-24 13:02:07 -0500

    Add files via upload

---
+++ stella/index.js
@@ -0,0 +1,10 @@
+/* Copyright (C) 2021 Mohamed Rizad
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
