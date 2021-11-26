c7dc626c9c50fdc786bdb9b17e7ff2b9f0c11613
Mohamed Rizad <rizaddq789654@gmail.com>
2021-11-26 10:58:51 -0500

    Delete Stella directory

--- Stella/index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    Base: require('./Base'),
-    Message: require('./Message'),
-    StringSession: require('./StringSession'),
-    ReplyMessage: require('./ReplyMessage'),
-    Image: require('./Image'),
-    Video: require('./Video')
-};
