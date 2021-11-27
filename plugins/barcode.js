b80234c6e2a5d645700540bb830374c935c36a42
Mohamed Rizad <rizaddq789654@gmail.com>
2021-11-27 02:12:46 -0500

    Add files via upload

---
+++ plugins/barcode.js
@@ -0,0 +1,19 @@
+const Rizad = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "*type some word after command🥲*"
+const conf = require('../config');
+let wk = conf.WORKTYPE == 'public' ? false : true
+
+    Rizad.tostella({ pattern: 'barcode ?(.*)', fromMe: wk,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/barcode_maker?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.AFN })
+
+    }));
+
