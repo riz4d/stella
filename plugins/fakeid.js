3164401cd39ccc7b0e8a8ef72b9b532ac48a07bf
Mohamed Rizad <rizaddq789654@gmail.com>
2021-12-09 13:10:42 -0500

    Update fakeid.js

--- plugins/fakeid.js
+++ plugins/fakeid.js
@@ -15,7 +15,7 @@ Rizad.tostella({pattern: 'fakeid ?(.*)', fromMe: true, desc: '' , dontAddCommand
     
 		const json = JSON.parse(response.body);
     
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  'NAME  :' + json.name + '\n' + 'GENDER :'+ json.gender + 'AGE  :' + json.age + '\n' + 'BDAY :'+ json.birtday + 'JOB  :' + json.occupation + '\n' + 'ADDRESS :'+ json.address +'PIN  :' + json.zip_code + '\n' + 'PLACE :'+ json.state + 'COUNTRY  :' + json.country + '\n' + 'PHONE :'+ json.phone , MessageType.text);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  '*NAME  :* ' + json.name + '\n' + '*GENDER :* '+ json.gender + '*AGE  :* ' + json.age + '\n' + '*BDAY :* '+ json.birtday + '*JOB  :* ' + json.occupation + '\n' + '*ADDRESS :* '+ json.address +'*PIN  :* ' + json.zip_code + '\n' + '*PLACE :* '+ json.state + '*COUNTRY  :* ' + json.country + '\n' + '*PHONE :* '+ json.phone , MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
@@ -32,7 +32,7 @@ Rizad.tostella({pattern: 'fakeid ?(.*)', fromMe: false, desc: '' , dontAddComman
     
 		const json = JSON.parse(response.body);
     
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  'NAME  :' + json.name + '\n' + 'GENDER :'+ json.gender + 'AGE  :' + json.age + '\n' + 'BDAY :'+ json.birtday + 'JOB  :' + json.occupation + '\n' + 'ADDRESS :'+ json.address +'PIN  :' + json.zip_code + '\n' + 'PLACE :'+ json.state + 'COUNTRY  :' + json.country + '\n' + 'PHONE :'+ json.phone , MessageType.text);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  '*NAME  :* ' + json.name + '\n' + '*GENDER :* '+ json.gender + '*AGE  :* ' + json.age + '\n' + '*BDAY :* '+ json.birtday + '*JOB  :* ' + json.occupation + '\n' + '*ADDRESS :* '+ json.address +'*PIN  :* ' + json.zip_code + '\n' + '*PLACE :* '+ json.state + '*ZIP CODE  :* ' + json.zip_code + '\n' + '*Email  :* ' + json.email + '\n' + '*PASSWORD  :* ' + json.password + '\n' + '*MARTIAL STATUS  :* ' + json.status + '\n' + '*COUNTRY  :* ' + json.country + '\n' + '*BLOOD GROUP  :* ' + json.blood_type + '\n' + '*HEIGHT  :* ' + json.height + '\n' + '*WEIGHT  :* ' + json.weight + '\n' + '*CREDIT CARD NUMBER  :* ' + json.card + '\n' + '*CVV  :* ' + json.code + '\n' + '*DATE  :* ' + json.date + '\n' + '*PHONE :* '+ json.phone , MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
