465d93431feb09f17c85859510d4ea6e17b8e5a6
Mohamed Rizad <rizaddq789654@gmail.com>
2021-12-09 23:12:08 -0500

    Update lub.js

--- plugins/lub.js
+++ plugins/lub.js
@@ -3,91 +3,11 @@ const {MessageType} = require('@adiwajshing/baileys');
 
 Rizad.tostella({pattern: 'love', fromMe: true}, (async (message, match) => {
 
-    await message.sendMessage("{\__/}\n( • - • )\n/>🍫 al sana çikolata");
+    await message.sendMessage("🍫 Luvv Yuh Too");
     
     await new Promise(r => setTimeout(r, 1500));
 
-    await message.sendMessage("{\__/}\n( • - • )\n🍫 < \  yada alma sende vardı");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>🍫 yada al kıyamadım");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>☕ al bu da yanında olsun");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>❤️ bunu da al ama kırma lütfen");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>💔 kırma demiştim");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n💔<\ kırdığın için üzgün olmalısın");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/> ❤️ yada al birtane daha");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>💔 amq malı !!!");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>❤️ bunu da al ama kırma lütfen yoksa s*kerim");
-
-}));
-
-Rizad.tostella({pattern: 'infocikolata', fromMe: true}, (async (message, match) => {
-
-    await message.sendMessage("Codded by *@riz4d*\nEğlenceli bir sevgi animasyonu :)\n💻Usage: *.love*");
-
-}));
-
-Rizad.tostella({pattern: 'love', fromMe: true}, (async (message, match) => {
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>🍫 ı give you chocolate");
-    
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n🍫 < \  or not. you already have");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>🍫 ı cant sacrifice you");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>☕ ı give you with this");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>❤️ take this too but don't break it, please");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>💔 ı said don't break it");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n💔<\ You must be sorry you did");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/> ❤️ or take another");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>💔 you stupid !!!");
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    await message.sendMessage("{\__/}\n( • - • )\n/>❤️ take this too but if you break it ı will **** you");
+    await message.sendMessage("\n Did you already have Any Crush ? ");
 
 }));
 
